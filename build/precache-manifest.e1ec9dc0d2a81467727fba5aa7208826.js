ed3f3daa37ae41dd5c3248c686827f00b3577f58
Evance Onyango <evaonyango251@gmail.com>
2021-10-22 09:01:43 +0300

    added build

---
+++ build/precache-manifest.e1ec9dc0d2a81467727fba5aa7208826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a109739229056e102df4c954e19c1f69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebe0699e5cc1a019757f",
+    "url": "/static/css/main.0c8489f7.chunk.css"
+  },
+  {
+    "revision": "66057293e9aff8f36eaf",
+    "url": "/static/js/2.1152b9cd.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.1152b9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebe0699e5cc1a019757f",
+    "url": "/static/js/main.77964a62.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
